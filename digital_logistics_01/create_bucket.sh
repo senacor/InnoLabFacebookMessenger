6df603efef2c7d220475de0a519a733445f463a2
kochp <philipp.koch@senacor.com>
2017-11-29 10:53:05 +0100

    add parcel json and s3 policy.

--- digital_logistics_01/create_bucket.sh
+++ digital_logistics_01/create_bucket.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+echo "create web bucket"
 aws s3api create-bucket --acl public-read --bucket digital-logistic-web --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
+
+echo "create parcel bucket"
+aws s3api create-bucket --acl private --bucket digital-logistic-parcel --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
