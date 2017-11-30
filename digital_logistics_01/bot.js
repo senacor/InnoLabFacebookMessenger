e064a8083b64ef28e11b488ad6831a6acb6dc816
Stefan Walter <stefan.walter@senacor.com>
2017-11-30 15:18:49 +0100

    Bot 01. Read parcel info from dynamodb

--- digital_logistics_01/bot.js
+++ digital_logistics_01/bot.js
@@ -1,28 +1,61 @@
+
 const botBuilder = require('claudia-bot-builder');
 const fbTemplate = botBuilder.fbTemplate;
 
+const doc = require('dynamodb-doc');
+const dynamoDb = new doc.DynamoDB();
+
+const buildTemplate = (data, queried_parcel_id) => {
+    console.log(data);
+    if (data.Items.length === 0) {
+        console.log(`could not find parcel_id: ${queried_parcel_id}`);
+        return 'Wir konnten zu der Auftragsnummer kein Paket finden.';
+    }
+    console.log('Found parcel!');
+    return new fbTemplate.List()
+    .addBubble('Sendungsstatus', `Auftragsnummer: ${data.Items[0].parcel_id}`)
+        .addImage(`https://s3.eu-central-1.amazonaws.com/digital-logistic-web/01_pacel_received_${data.Items[0].Status[0].Status}.png`)
+    .addBubble('Transport zum Paktezentrum', `${data.Items[0].Status[1].description}`)
+        .addImage(`https://s3.eu-central-1.amazonaws.com/digital-logistic-web/02_parcel_transport_${data.Items[0].Status[1].Status}.png`)
+    .addBubble('Bearbeitung im Paketzentrum', `${data.Items[0].Status[2].description}`)
+        .addImage(`https://s3.eu-central-1.amazonaws.com/digital-logistic-web/03_parcel_factroy_${data.Items[0].Status[2].Status}.png`)
+    .addBubble('In Zustellung', `${data.Items[0].Status[3].description}`)
+        .addImage(`https://s3.eu-central-1.amazonaws.com/digital-logistic-web/04_parcel_delivery_${data.Items[0].Status[3].Status}.png`)
+    .get()
+}
+
 const parser = request => {
-    const matchStatus = request.text.match(/status\D*(\d+)/i);
-    if (matchStatus) {
-        const matchParcelNumber = matchStatus[1].match(/(\d{8})\D*/);
-        if (matchParcelNumber) {
-            return new fbTemplate.List()
-            .addBubble('Sendungsstatus', `Auftragsnummer: ${matchParcelNumber[1]}`)
-                .addImage('https://s3.eu-central-1.amazonaws.com/digital-logistic-web/01_pacel_received_done.png')
-            .addBubble('Transport zum Paktezentrum')
-                .addImage('https://s3.eu-central-1.amazonaws.com/digital-logistic-web/02_parcel_transport_done.png')
-            .addBubble('Bearbeitung im Paketzentrum')
-                .addImage('https://s3.eu-central-1.amazonaws.com/digital-logistic-web/03_parcel_factroy_in_progress.png')
-            .addBubble('In Zustellung')
-                .addImage('https://s3.eu-central-1.amazonaws.com/digital-logistic-web/04_parcel_delivery_open.png')    
-            .get();
-        } else {
-            return 'Ungültige Auftragsnummer. Die Auftragsnummer ist 8-stellig.'
-        } 
-    }
-    else {
-        return 'Wir konnten Ihre Anfrage leider nicht verstehen.'
-    }
+    return new Promise(resolve => {
+        console.log(request);
+        const matchStatus = request.text.match(/status\D*(\d+)/i);
+        if (matchStatus) {
+            const matchParcelNumber = matchStatus[1].match(/(\d{8})\D*/);
+            if (matchParcelNumber) {
+                console.log(`Search in dynamoDb for parcel_id ${matchParcelNumber[1]}`);
+                dynamoDb.query({
+                    ExpressionAttributeValues: {
+                       ":v1": matchParcelNumber[1]
+                    },
+                KeyConditionExpression: "parcel_id = :v1",
+                TableName: "digital_logistics_parcel"
+                }, (err, data) => {
+                    if (err) {
+                        console.log(`could not find parcel_id: ${err}`);
+                        return resolve("Wir konnten zu der Auftragsnummer kein Paket finden.");
+                    }
+
+                    return resolve(buildTemplate(data, matchParcelNumber[1]));
+                });
+            } else {
+                console.log('Invalid parcel id');
+                return resolve('Ungültige Auftragsnummer. Die Auftragsnummer ist 8-stellig.')
+            } 
+        }
+        else {
+            console.log('could not understand request');
+            return resolve('Wir konnten Ihre Anfrage leider nicht verstehen.')
+        }
+    });
 };
 
 
