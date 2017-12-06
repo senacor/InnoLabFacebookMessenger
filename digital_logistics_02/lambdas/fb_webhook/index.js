e57ab6999d642eb6858af608b52804b649b58e26
Marlon Alagoda <marlon.alagoda@senacor.com>
2017-12-06 09:01:20 +0100

    Basic login- and logout process

--- digital_logistics_02/lambdas/fb_webhook/index.js
+++ digital_logistics_02/lambdas/fb_webhook/index.js
@@ -1,41 +1,162 @@
 const botBuilder = require('claudia-bot-builder')
 const {fbTemplate} = require('claudia-bot-builder')
 const objectPath = require('object-path')
-const aws = require('aws-sdk')
+const AWS = require('aws-sdk')
+const DOC = require('dynamodb-doc')
 
-const lambda = new aws.Lambda({
-    region: 'eu-central-1'
+AWS.config.update({region: 'eu-central-1'})
+const docClient = new DOC.DynamoDB()
+
+/**
+ * Returns a single user, queried by authorization code
+ * @param {String} authCode 
+ * @returns Promise.<{Object}> user from db
+ */
+const getUserByAuthorizationCode = authCode => new Promise((resolve, reject) => {
+  const query = {
+    TableName: 'digital_logistics_customer',
+    ExpressionAttributeValues: {
+      ':authorization_code': authCode
+    },
+    FilterExpression: 'authorization_code = :authorization_code'
+  }
+
+  docClient.scan(query, (err, data) => {
+    if (err) {
+      return reject(err)
+    }
+
+    const users = data.Items
+
+    if (users.length !== 1) {
+      return reject(new Error(`Unexpected amount (${users.length}) of users for auth code ${authCode}`))
+    }
+
+    return resolve(users[0])
+  })
 })
 
 /**
- * Returns a promise, which gets resolved after the lambda named @param name get executed successfully.
- * @param name the lambda's name
- * @param payload JSON payload which gets passed to lambda
- * @returns Promise
+ * Returns a single user, queried by fb psid
+ * @param {String} psid 
+ * @returns Promise.<{Object}> user from db
  */
-const invokeLambda = (name = '', payload = {}) => new Promise((resolve, reject) => {
-    lambda.invoke({
-        FunctionName: name,
-        Payload: JSON.stringify(payload)
-    }, err => {
-        if(err) {
-            console.log(err)
-            reject(err)
-        }
+const getUserByPsid = psid => new Promise((resolve, reject) => {
+  const query = {
+    TableName: 'digital_logistics_customer',
+    ExpressionAttributeValues: {
+      ':fb_psid': psid
+    },
+    FilterExpression: 'fb_psid = :fb_psid'
+  }
 
-        resolve()
-    }) 
+  docClient.scan(query, (err, data) => {
+    if (err) {
+      return reject(err)
+    }
+
+    const users = data.Items
+
+    if (users.length !== 1) {
+      return reject(new Error(`Unexpected amount (${users.length}) of users for psid code ${psid}`))
+    }
+
+    return resolve(users[0])
+  })
 })
 
-module.exports = botBuilder(request => new Promise((resolve, reject) => {
-    if(objectPath.has(request, 'originalRequest.message')) {
-        resolve(new fbTemplate.Button('Login because, bla bla bla')
-            .addLoginButton('https://s3.eu-central-1.amazonaws.com/digital-logistic-web/index.html')
-            .get())
-    } else if(objectPath.has(request, 'originalRequest.account_linking')) {
-        return invokeLambda('finish_linking')
-    } else {
-        console.log('Unexpected event')
-        reject()
+/**
+ * Sets psid and auth code in db to given values
+ * @param {Object} user 
+ * @returns Promise.<{Object}> resolves with an object of all new user's attributes
+ */
+const setPsidAndAuthCode = user => new Promise((resolve, reject) => {
+  const update = {
+    TableName: 'digital_logistics_customer',
+    Key: {
+      'email': user.email,
+      'customer_id': user.customer_id
+    },
+    UpdateExpression: 'set authorization_code = :r, fb_psid = :f',
+    ExpressionAttributeValues: {
+      ':r': user.authorization_code,
+      ':f': user.fb_psid
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+
+  docClient.updateItem(update, (err, data) => {
+    if (err) {
+      return reject(err)
     }
-}), {platforms: ['facebook']})
+
+    return resolve(data)
+  })
+})
+
+/**
+ * This function removes a given field at digital_logistics_customer for a given user
+ * @param {Object} user user object containing at least email and customer_id
+ * @param {*} field field to remove in db
+ */
+const removeFieldInDb = (user, field) => new Promise((resolve, reject) => {
+  const remove = {
+    TableName: 'digital_logistics_customer',
+    Key: {
+      'email': user.email,
+      'customer_id': user.customer_id
+    },
+    UpdateExpression: `remove ${field}`
+  }
+
+  docClient.updateItem(remove, (err, data) => {
+    if (err) {
+      return reject(err)
+    }
+
+    return resolve(data)
+  })
+})
+
+module.exports = botBuilder(request => {
+  if (objectPath.has(request, 'originalRequest.message')) {
+    const message = objectPath.get(request, 'originalRequest.message.text')
+
+    if (message.includes('login')) {
+      return Promise.resolve(new fbTemplate.Button('Login because bla bla bla')
+        .addLoginButton('https://s3.eu-central-1.amazonaws.com/digital-logistic-web/login.html')
+        .get())
+    } else if (message.includes('logout')) {
+      return Promise.resolve(new fbTemplate.Button('Logout because blu blu blu')
+        .addLogoutButton()
+        .get())
+    } else {
+      return Promise.resolve('Hi there')
+    }
+  } else if (objectPath.has(request, 'originalRequest.account_linking') && objectPath.get(request, 'originalRequest.account_linking.status') === 'linked') {
+    console.log('Received linking event:', JSON.stringify(request))
+
+    const authCode = objectPath.get(request, 'originalRequest.account_linking.authorization_code')
+    if (!authCode || authCode === '') {
+      console.log('No or empty auth code provided!')
+      return Promise.reject()
+    }
+
+    return getUserByAuthorizationCode(authCode)
+      .then(user => {
+        user.fb_psid = objectPath.get(request, 'originalRequest.sender.id')
+
+        return user
+      })
+      .then(user => setPsidAndAuthCode(user).then(() => user))
+      .then(user => removeFieldInDb(user, 'authorization_code'))
+      .then(() => null) // Return nothing
+  } else if (objectPath.has(request, 'originalRequest.account_linking') && objectPath.get(request, 'originalRequest.account_linking.status') === 'unlinked') {
+    return getUserByPsid(objectPath.get(request, 'originalRequest.sender.id'))
+      .then(user => removeFieldInDb(user, 'fb_psid'))
+      .then(() => null) // Return nothing
+  } else {
+    console.log('Unexpected event')
+    return Promise.reject()
+  }
+}, {platforms: ['facebook']})
--- digital_logistics_02/lambdas/user_login/index.js
+++ digital_logistics_02/lambdas/user_login/index.js
@@ -1,18 +1,100 @@
 const ApiBuilder = require('claudia-api-builder')
+const objectPath = require('object-path')
+const AWS = require('aws-sdk')
+const DOC = require('dynamodb-doc')
+
+AWS.config.update({region: 'eu-central-1'})
 
 const api = new ApiBuilder({mergeVars: true})
+const docClient = new DOC.DynamoDB()
 
-api.post('/', req => {
-    let data
-    try {
-        data = JSON.parse(req.body)
-    } catch(e) {
-        return new Error('Could not parse payload')
+/**
+ * Finds user from our dynamo db by email
+ * Rejects if no or to many users found or if db returns error
+ * @param {String} email 
+ * @returns Promise.<{Object}> resolves with user
+ */
+const findUser = email => new Promise((resolve, reject) => {
+  const query = {
+    TableName: 'digital_logistics_customer',
+    IndexName: 'email-index',
+    KeyConditionExpression: 'email = :hkey',
+    ExpressionAttributeValues: {
+      ':hkey': email
+    }
+  }
+
+  docClient.query(query, (err, data) => {
+    if (err) {
+      return reject(err)
     }
 
-    console.log(data)
+    const users = data.Items
 
-    return JSON.stringify({authorizationCode: '123'})
+    if (users.length !== 1) {
+      return reject(`Unexpected amount (${users.length}) of users for email ${email}`)
+    }
+
+    return resolve(users[0])
+  })
 })
 
+/**
+ * Generates random 10 sized string and saves it to as authorization_code to our dynamo db
+ * @param {Object} user 
+ * @returns Promise.<{Object}> resolves with an object containing all user's attributes
+ */
+const setAuthCode = user => new Promise((resolve, reject) => {
+  const update = {
+    TableName: 'digital_logistics_customer',
+    Key: {
+      'email': user.email,
+      'customer_id': user.customer_id
+    },
+    UpdateExpression: 'set authorization_code = :r',
+    ExpressionAttributeValues: {
+      ':r': [...Array(10)].map(() => Math.random().toString(36)[3]).join('') // 10 random chars: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+
+  docClient.updateItem(update, (err, data) => {
+    if (err) {
+      return reject(err)
+    }
+
+    return resolve(data)
+  })
+})
+
+/**
+ * Finds user by request data; checks pw; creates and saves auth code and returns with auth code
+ * @param {Object} req HTTP request object
+ * @returns Promise.<{{authorizationCode: String}|{error: String}}> resolves with an object containing the authorization code or an error message
+ */
+const requestHandler = req => {
+  let data
+  try {
+    data = JSON.parse(req.body)
+  } catch (e) {
+    return JSON.stringify({error: 'Could not parse payload'})
+  }
+
+  return findUser(data.email)
+    .then(user => {
+      if (user.password !== data.password) {
+        throw new Error('Password incorrect')
+      }
+      return user
+    })
+    .then(user => setAuthCode(user))
+    .then(updateResult => ({authorizationCode: objectPath.get(updateResult, 'Attributes.authorization_code')}))
+    .catch(err => {
+      console.log(err)
+      return {error: 'Could not login'}
+    })
+    .then(msg => JSON.stringify(msg))
+}
+
+api.post('/', req => requestHandler(req))
 module.exports = api
