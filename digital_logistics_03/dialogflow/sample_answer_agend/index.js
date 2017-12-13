b288a1cfc15be1bf722f814aeb79fc79ad683369
kochp <philipp.koch@senacor.com>
2017-12-13 12:26:59 +0100

    use a more flat structure to give back the results.

--- digital_logistics_03/dialogflow/sample_answer_agend/index.js
+++ digital_logistics_03/dialogflow/sample_answer_agend/index.js
@@ -27,47 +27,46 @@ const dialogflowEventHandler = req => {
                     "status": "in Auslieferung",
                     "parcel_id": String(req.body.result.parameters.parcel_id.parcel_id),
                     "icon": "04_parcel_delivery_in_progress.png",
-                    "status_list": {
-                        "received": {
-                            "description":
-                                "Paket am 28.11.2017 an Digital Logistics übergeben",
-                            "Status":
-                                "done",
-                            "type":
-                                "received",
-                            "icon_prefix":
-                                "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/01_pacel_received_done.png"
-                        },
-                        "transport": {
-                            "description":
-                                "Transport am 29.11.2017 in Warenlager Bonn",
-                            "Status":
-                                "done",
-                            "type":
-                                "transport",
-                            "icon_prefix":
-                                "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/02_parcel_transport_done.png"
-                        },
-                        "factory": {
-                            "description":
-                                "Verarbeitung am Warenhaus am 30.11.2017",
-                            "Status":
-                                "done",
-                            "type":
-                                "factory",
-                            "icon_prefix":
-                                "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/03_parcel_factroy_done.png"
-                        },
-                        "delivery": {
-                            "description":
-                                "Auslieferung am 01.12.2017 erfolgreich",
-                            "Status":
-                                "done",
-                            "type":
-                                "delivery",
-                            "icon_prefix":
-                                "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/04_parcel_delivery_done.png"
-                        }
+
+                    "received": {
+                        "description":
+                            "Paket am 28.11.2017 an Digital Logistics übergeben",
+                        "Status":
+                            "done",
+                        "type":
+                            "received",
+                        "icon_prefix":
+                            "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/01_pacel_received_done.png"
+                    },
+                    "transport": {
+                        "description":
+                            "Transport am 29.11.2017 in Warenlager Bonn",
+                        "Status":
+                            "done",
+                        "type":
+                            "transport",
+                        "icon_prefix":
+                            "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/02_parcel_transport_done.png"
+                    },
+                    "factory": {
+                        "description":
+                            "Verarbeitung am Warenhaus am 30.11.2017",
+                        "Status":
+                            "done",
+                        "type":
+                            "factory",
+                        "icon_prefix":
+                            "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/03_parcel_factroy_done.png"
+                    },
+                    "delivery": {
+                        "description":
+                            "Auslieferung am 01.12.2017 erfolgreich",
+                        "Status":
+                            "done",
+                        "type":
+                            "delivery",
+                        "icon_prefix":
+                            "https://s3.eu-central-1.amazonaws.com/digital-logistic-web/04_parcel_delivery_done.png"
                     }
                 },
                 "name": "fill_slots"
