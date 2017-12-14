59dd21a64c015107d98b54c0d11ee14fb00082e7
Marlon Alagoda <marlon.alagoda@senacor.com>
2017-12-14 10:44:36 +0100

    debug stuff

--- digital_logistics_03/dialogflow/sample_answer_agend/src/action_handler/status_details.js
+++ digital_logistics_03/dialogflow/sample_answer_agend/src/action_handler/status_details.js
@@ -1,49 +1,7 @@
-console.log('FIRST FU LINE')
+console.log('hier1')
+const aws = require('aws-sdk')
+console.log('hier2')
+aws.config.update({region: 'eu-central-1'})
+console.log('hier3')
 
-const objectPath = require('object-path')
-console.log('4 FU LINE')
-const db = require('../db')
-console.log('6 FU LINE')
-
-const eventName = 'status_details'
-
-console.log('8 FU LINE')
-
-module.exports = (req, api) => {
-    console.log('hierhierhierhierhier')
-    // SENDER ID: console.log(req.body.originalRequest.data.sender.id)
-
-    const parcelId = objectPath.get(req, 'result.parameters.parcel_id.parcel_id')
-    let calculateResult = () => {
-        console.log('hier2')
-        return Promise.resolve({})
-    }
-
-    console.log('hier')
-
-    if (req.body.result.resolvedQuery !== eventName) {
-        console.log('hier1')
-        calculateResult = () => {
-            console.log('hier4')
-            return db.getParcels(parcelId)
-                .then(parcels => {
-                    console.log('hier5')
-                    console.log(parcels)
-                    const result = {
-                        followupEvent: {
-                            data: { }
-                        }
-                    }
-
-                    return result
-                })
-        }
-    }
-
-    console.log('hier3')
-
-    return calculateResult()
-        .then(result => new api.ApiResponse(result, {'Content-Type': 'application/json'}, 200))
-}
-
-console.log('LAST FU LINE')
+module.exports = () => console.log('HIIII')
