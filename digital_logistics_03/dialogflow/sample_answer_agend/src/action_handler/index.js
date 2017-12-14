8fa65a27a2e93374c71713a4c638a5ee0da18184
Marlon Alagoda <marlon.alagoda@senacor.com>
2017-12-14 11:48:23 +0100

    dont load handler dynamically

---
+++ digital_logistics_03/dialogflow/sample_answer_agend/src/action_handler/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    parcel_location: require('./parcel_location'),
+    parcel_receive_time: require('./parcel_receive_time'),
+    status_details: require('./status_details')
+}
