fb890e3e6f2412139ba6dc473defb5a53151efbb
kochp <philipp.koch@senacor.com>
2017-12-15 12:08:41 +0100

    Add map location to parcel receive time

--- digital_logistics_03/dialogflow/sample_answer_agend/src/action_handler/parcel_receive_time.js
+++ digital_logistics_03/dialogflow/sample_answer_agend/src/action_handler/parcel_receive_time.js
@@ -8,6 +8,7 @@ module.exports = (req, api) => {
                     delivery_date: '15.12.2017',
                     image_header: 'https://s3.eu-central-1.amazonaws.com/digital-logistic-web/00_pacel_images_header.png',
                     parcel_id: String(req.body.result.parameters.parcel_id.parcel_id),
+                    map_location: 'Koeln+Leichweg+22'
                 },
                 name: eventLoopSuppression.getFillSlotsEventName(req)
             }
